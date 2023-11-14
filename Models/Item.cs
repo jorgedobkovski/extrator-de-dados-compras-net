144c4bef0d33b7de8fbd791d63b0521ec05d133c
Jorge Dobkovski <47338154+jorgedobkovski@users.noreply.github.com>
2023-11-14 14:12:14 -0400

    initial commit

---
+++ Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extratorTermoHomologacaoComprasNet.Models
+{
+    public class Item
+    {
+        public int Numero { get; set; }
+        public string? Descricao { get; set; }
+        public string? DescricaoDetalhada { get; set; }
+        public decimal? ValorEstimado { get; set; }
+        public string? UnidadeFornecimento { get; set; }
+        public string? Situacao { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using extratorTermoHomologacaoComprasNet.Models;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.Text;
+
+using (PdfReader leitor = new PdfReader("C:/temp/pdf/termo.pdf"))
+{
+    StringBuilder texto = new StringBuilder();
+    for (int i = 1; i <= leitor.NumberOfPages; i++)
+    {
+        texto.Append(PdfTextExtractor.GetTextFromPage(leitor, i));
+    }
+
+    var conteudoPdf = texto.ToString();
+    //Console.WriteLine(conteudoPdf);
+
+    var item = new Item();
+
+    //Isolando informações do Item a partir de Eventos da Compra até Proposta do Item:
+
+    var indexTituloEventosDaCompra = conteudoPdf.IndexOf("\nEventos da compra\nData/Hora Descrição\n");
+
+    var indexTituloPropostasDoItem = conteudoPdf.IndexOf("\nPropostas do Item ");
+
+    var conteudoBrutoContendoInformacoesDoItem = "";
+
+    if (indexTituloEventosDaCompra >= 0 && indexTituloPropostasDoItem >= 0)
+    {
+        // Calcula o comprimento da substring entre os dois índices
+        int startIndex = indexTituloEventosDaCompra + "\nEventos da compra\nData/Hora Descrição\n".Length;
+        int length = indexTituloPropostasDoItem - startIndex;
+
+        // Extrai a substring
+        conteudoBrutoContendoInformacoesDoItem = conteudoPdf.Substring(startIndex, length);
+    }
+    else
+    {
+        Console.WriteLine("Índices não encontrados. ETAPA: Coletando informações brutas a entre os eventos da compra e as propostas do item.");
+    }
+
+    //Separando as informações dos itens
+    int indexTituloItemNumero = -1;
+    var conteudoDoItem = "";
+
+    if (conteudoBrutoContendoInformacoesDoItem != "")
+    {
+        indexTituloItemNumero = conteudoBrutoContendoInformacoesDoItem.IndexOf("\nItem ");
+        conteudoDoItem = conteudoBrutoContendoInformacoesDoItem.Substring(indexTituloItemNumero);
+    } else
+    {
+        Console.WriteLine("Índices não encontrados. ETAPA: Separando as informações dos itens das informações brutas (eventos da compra - propostas)");
+    }
+
+    Console.WriteLine(conteudoDoItem);
+    Console.ReadLine();
+}
